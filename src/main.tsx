01fe79ca9bcccf73f7059741c39b415631bd5254
PolarBowler <39461054+PolarBowler@users.noreply.github.com>
2024-09-13 22:18:34 +0200

    fix component naming to be pascal case

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import RouterProvider from "~/providers/router-provider.tsx";
+import RouterProvider from "~/providers/RouterProvider";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
--- src/providers/RouterProvider.tsx
+++ src/providers/RouterProvider.tsx
@@ -1,70 +1,70 @@
 import { createBrowserRouter, RouterProvider as ReactRouterProvider } from "react-router-dom";
 
 import Routes from "#/routes";
-import RootErrorBoundary from "~/components/router/root-error-boundary";
+import RootErrorBoundary from "~/components/router/RootErrorBoundary";
 import { lazyRoute } from "~/utils/lazy-route-helper";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        lazy: lazyRoute(() => import("~/routes/root")),
+        lazy: lazyRoute(() => import("~/routes/Root")),
         errorElement: <RootErrorBoundary />,
         children: [
             {
                 path: Routes.Home,
-                lazy: lazyRoute(() => import("~/routes")),
+                lazy: lazyRoute(() => import("~/routes/Home")),
             },
             {
                 path: Routes.Login,
-                lazy: lazyRoute(() => import("~/routes/login")),
+                lazy: lazyRoute(() => import("~/routes/Login")),
             },
             {
                 path: Routes.Registration,
-                lazy: lazyRoute(() => import("~/routes/registration")),
+                lazy: lazyRoute(() => import("~/routes/Registration")),
             },
             {
                 path: Routes.User,
-                lazy: lazyRoute(() => import("~/routes/user")),
+                lazy: lazyRoute(() => import("~/routes/User")),
             },
             {
                 path: Routes.UserChangeEmail,
-                lazy: lazyRoute(() => import("~/routes/user/change-email")),
+                lazy: lazyRoute(() => import("~/routes/User/ChangeEmail")),
             },
             {
                 path: Routes.UserResetPassword,
-                lazy: lazyRoute(() => import("~/routes/user/reset-password")),
+                lazy: lazyRoute(() => import("~/routes/User/ResetPassword")),
             },
             {
                 path: Routes.Templates,
-                lazy: lazyRoute(() => import("~/routes/templates")),
+                lazy: lazyRoute(() => import("~/routes/Templates")),
             },
             {
                 path: Routes.TemplatesEdit,
-                lazy: lazyRoute(() => import("~/routes/templates/edit")),
+                lazy: lazyRoute(() => import("~/routes/Templates/Edit")),
             },
             {
                 path: Routes.Entries,
-                lazy: lazyRoute(() => import("~/routes/entries")),
+                lazy: lazyRoute(() => import("~/routes/Entries")),
             },
             {
                 path: Routes.EntriesEdit,
-                lazy: lazyRoute(() => import("~/routes/entries/edit")),
+                lazy: lazyRoute(() => import("~/routes/Entries/Edit")),
             },
             {
                 path: Routes.Admin,
-                lazy: lazyRoute(() => import("~/routes/admin")),
+                lazy: lazyRoute(() => import("~/routes/Admin")),
             },
             {
                 path: Routes.AdminManageThemes,
-                lazy: lazyRoute(() => import("~/routes/admin/manage-themes")),
+                lazy: lazyRoute(() => import("~/routes/Admin/ManageThemes")),
             },
             {
                 path: Routes.AdminManageTranslations,
-                lazy: lazyRoute(() => import("~/routes/admin/manage-translations")),
+                lazy: lazyRoute(() => import("~/routes/Admin/ManageTranslations")),
             },
             {
                 path: Routes.AdminManageUsers,
-                lazy: lazyRoute(() => import("~/routes/admin/manage-users")),
+                lazy: lazyRoute(() => import("~/routes/Admin/ManageUsers")),
             },
         ],
     },
