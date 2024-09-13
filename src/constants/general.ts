f3240af331cebf957e45f455725573094b777857
PolarBowler <39461054+PolarBowler@users.noreply.github.com>
2024-09-13 13:44:23 +0200

    config additions - add .env-files + intellisense for env - add constants folder + general constants file - configure import aliases

---
+++ src/constants/general.ts
@@ -0,0 +1,4 @@
+/**
+ * TODO This should point towards a dev-server for backend in dev and to the live domain in prod
+ */
+export const backendURL = import.meta.env.BACKEND_URL;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    resolve: {
+        alias: {
+            "~": path.resolve(__dirname, "./src"),
+            "@const": path.resolve(__dirname, "./src/constants"),
+        },
+    },
     plugins: [react()],
 });
