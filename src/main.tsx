eb71f65f8cb8c2ef79a1ab53c34dc0ae1aa7b1b5
PolarBowler <39461054+PolarBowler@users.noreply.github.com>
2024-09-19 10:26:08 +0200

    add base setup for redux

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import RouterProvider from "~/providers/RouterProvider";
+import ReduxProvider from "./providers/ReduxProvider";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <RouterProvider />
+        <ReduxProvider>
+            <RouterProvider />
+        </ReduxProvider>
     </StrictMode>,
 );
