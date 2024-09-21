da8a004bd47b630bc53837bc23a397511a499523
PolarBowler <f.windisch97@gmail.com>
2024-09-21 16:42:13 +0200

    document the wait function

--- src/utils/wait.ts
+++ src/utils/wait.ts
@@ -1,4 +1,11 @@
-export default function (ms: number): Promise<void> {
+/**
+ * An async function that resolves after the given milliseconds.
+ *
+ * @deprecated ONLY USE THIS FOR TESTING. IF YOU USE THIS IN ACTUAL CODE YOU WILL BE FIRED!
+ * @param ms Milliseconds until the Promise resolves
+ * @returns A promises that resolves after the given milliseconds.
+ */
+export default function (ms: number = 0): Promise<void> {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve();
