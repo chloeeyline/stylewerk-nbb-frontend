7fd089ddee3ba38edf7658e4b3e3690005b0d9c0
PolarBowler <39461054+PolarBowler@users.noreply.github.com>
2024-09-25 19:58:24 +0200

    add error-reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import userReducer from "./features/user/userSlice";
+import errorReducer from "./features/error/errorSlice";
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
+        error: errorReducer,
     },
 });
 
