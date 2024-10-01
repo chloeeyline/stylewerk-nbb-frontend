4c31648e7a06436e579eb49d9835a6a8e1b170c6
PolarBowler <39461054+PolarBowler@users.noreply.github.com>
2024-10-01 15:52:47 +0200

    cleanup routeparams

--- src/constants/route-params.ts
+++ src/constants/route-params.ts
@@ -1,15 +1,7 @@
 /**
  * These are the RouteParams for reuseability!
  */
-/* export const enum RouteParams {
-    TemplateId = ":templateId",
-    EntryId = ":entryId",
-    UserId = ":userId",
-    TranslationId = ":translationId",
-    ThemeId = ":themeId",
-} */
-
-export const RouteParams = {
+export default {
     TemplateId: ":templateId",
     EntryId: ":entryId",
     UserId: ":userId",
