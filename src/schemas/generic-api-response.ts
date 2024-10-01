181c686a44f3075f60561dd37fcb55c2d8343be6
PolarBowler <39461054+PolarBowler@users.noreply.github.com>
2024-10-01 10:39:23 +0200

    update generic api schema to current schema

--- src/schemas/generic-api-response.ts
+++ src/schemas/generic-api-response.ts
@@ -1,18 +1,12 @@
 import { z } from "zod";
 
-const makeApiSchema = <T>(schema: z.ZodType<T>) => {
-    return z.object({
-        type: z.number().int().safe(),
-        typeText: z.string(),
-        errorCode: z.number().int().safe().nullable(),
-        errorText: z.string().nullable(),
-        data: schema,
-    });
-};
-
-const genericApiSchema = makeApiSchema(z.unknown());
+const genericApiSchema = z.object({
+    code: z.number().int().safe(),
+    codeName: z.string(),
+    data: z.unknown(),
+});
 
 type GenericApi = z.infer<typeof genericApiSchema>;
 
 export type { GenericApi };
-export { makeApiSchema, genericApiSchema };
+export { genericApiSchema };
