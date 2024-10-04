e1846478a654dd8ca0402686d4d4bd43005238dd
chloeeyline <chloe.eyline.hauer@gmail.com>
2024-10-04 09:35:29 +0200

    Filter implemented

--- src/routes/Templates/Layout.tsx
+++ src/routes/Templates/Layout.tsx
@@ -1,10 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+import type React from "react";
 
 import Grid from "~/components/layout/Grid";
 import ScrollContainer from "~/components/layout/ScrollContainer";
 import { useAppDispatch, useAppSelector } from "~/redux/hooks";
-import { listTemplates, selectTemplate } from "~/redux/features/template/template-slice";
+import {
+    listTemplates,
+    selectTemplate,
+    setFilter,
+    setHideFilters,
+    setHideList,
+} from "~/redux/features/template/template-slice";
 import { useEffect } from "react";
+import RouteParams from "~/constants/route-params";
+import Routes from "~/constants/routes";
+import cls from "~/utils/class-name-helper";
 
 const TemplatesList = () => {
     const template = useAppSelector(selectTemplate);
@@ -15,28 +25,132 @@ const TemplatesList = () => {
     }, []);
 
     return (
-        <div>
+        <ScrollContainer
+            direction="vertical"
+            className={cls("p-1", template.hideList ? "hidden" : undefined)}>
             {template.items &&
                 template.items.length > 0 &&
                 template.items?.map((item) => (
-                    <div key={item.id}>
-                        {item.name && <div>{item.name}</div>}
-                        {item.description && <div>{item.description}</div>}
-                        {item.tags && <div>{item.tags}</div>}
-                        {item.username && <div>{item.username}</div>}
-                    </div>
+                    <NavLink
+                        to={Routes.Templates.View.replace(RouteParams.TemplateId, item.id)}
+                        key={item.id}
+                        className="lcontainer m-be-1">
+                        <div className="lrow">
+                            {item.name && <div className="lcell">{item.name}</div>}
+                            {item.username && (
+                                <div className="lcell" style={{ textAlign: "right" }}>
+                                    {item.username}
+                                </div>
+                            )}
+                        </div>
+                        {item.description && (
+                            <div className="lrow">
+                                <div className="lcell">{item.description}</div>
+                            </div>
+                        )}
+                        {item.tags && (
+                            <div className="lrow">
+                                {item.tags.split(",").map((tag) => (
+                                    <div key={tag} className="lcell tag">
+                                        {tag}
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                    </NavLink>
                 ))}
-        </div>
+        </ScrollContainer>
     );
 };
 
 export default function TemplatesLayout() {
+    const template = useAppSelector(selectTemplate);
+    const dispatch = useAppDispatch();
+
+    const dispatchFilter = (e: React.ChangeEvent<HTMLInputElement>) => {
+        dispatch(
+            setFilter({
+                type: e.target.name,
+                value: e.target.value,
+            }),
+        );
+    };
+
     return (
-        <Grid layout="sidebarStart" className="size-block-100 gap" style={{ "--gap": "1rem" }}>
-            <TemplatesList />
-            <ScrollContainer direction="vertical">
-                <Outlet />
-            </ScrollContainer>
+        <Grid layout="header" className="size-block-100 gap" style={{ "--gap": "1rem" }}>
+            <div>
+                <button onClick={() => dispatch(listTemplates())}>Refresh</button>
+                <button
+                    onClick={() => {
+                        dispatch(setHideList());
+                    }}>
+                    {template.hideList ? "Liste anzeigen" : "Liste verstecken"}
+                </button>
+                <button
+                    onClick={() => {
+                        dispatch(setHideFilters());
+                    }}>
+                    {template.hideFilters ? "Filter anzeigen" : "Filter verstecken"}
+                </button>
+                <form className={cls("header", template.hideFilters ? "hidden" : undefined)}>
+                    <fieldset className="header">
+                        <legend>Filter</legend>
+                        <label htmlFor="name">Name</label>
+                        <input
+                            name="name"
+                            type="text"
+                            value={template.filter.name}
+                            onChange={dispatchFilter}
+                        />
+                        <label htmlFor="description">Beschreibung</label>
+                        <input
+                            name="description"
+                            type="text"
+                            value={template.filter.description}
+                            onChange={dispatchFilter}
+                        />
+                        <label htmlFor="tags">Tags</label>
+                        <input
+                            name="tags"
+                            type="text"
+                            value={template.filter.tags}
+                            onChange={dispatchFilter}
+                        />
+                        <label htmlFor="username">Benutzername</label>
+                        <input
+                            name="username"
+                            type="text"
+                            value={template.filter.username}
+                            onChange={dispatchFilter}
+                        />
+                    </fieldset>
+                    <fieldset style={{ display: "grid" }}>
+                        <legend>Sichtbarkeit</legend>
+                        <div>
+                            <input name="owned" type="checkbox" />
+                            <label htmlFor="owned">Eigene</label>
+                        </div>
+                        <div>
+                            <input name="shared" type="checkbox" />
+                            <label htmlFor="shared">Geteilt</label>
+                        </div>
+                        <div>
+                            <input name="public" type="checkbox" />
+                            <label htmlFor="public">Öffentliche</label>
+                        </div>
+                        <div>
+                            <input name="directUser" type="checkbox" />
+                            <label htmlFor="directUser">Genauer Benutzername</label>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+            <Grid layout="sidebarStart" className="size-block-100 gap" style={{ "--gap": "1rem" }}>
+                <TemplatesList />
+                <ScrollContainer direction="vertical">
+                    <Outlet />
+                </ScrollContainer>
+            </Grid>
         </Grid>
     );
 }
