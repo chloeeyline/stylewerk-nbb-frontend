7a6c460b6217d47dacecf9d81c8e8a81f296e055
chloeeyline <chloe.eyline.hauer@gmail.com>
2024-10-06 13:46:57 +0200

    Adjusted imports

--- src/redux/features/editor/editor-slice.ts
+++ src/redux/features/editor/editor-slice.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import Backend from "~/constants/backend-routes";
 import type { AppDispatch, RootState } from "~/redux/store";
 import Ajax from "~/utils/ajax";
