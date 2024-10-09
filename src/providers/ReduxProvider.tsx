cd8e8aea42b29ba67593f6017daef38fdc7f169c
PolarBowler <39461054+PolarBowler@users.noreply.github.com>
2024-10-09 13:44:57 +0200

    fix glitchy loading of user

--- src/providers/ReduxProvider.tsx
+++ src/providers/ReduxProvider.tsx
@@ -1,16 +1,23 @@
 import type React from "react";
-import { memo } from "react";
+import { memo, useEffect } from "react";
 import { Provider } from "react-redux";
 
-import { getUser } from "~/redux/features/user/user-slice";
-import { useAppDispatch } from "~/redux/hooks";
+import { refreshUser, selectUser } from "~/redux/features/user/user-slice";
+import { useAppDispatch, useAppSelector } from "~/redux/hooks";
 import { store } from "~/redux/store";
 
 const UserCaller = memo(({ children }: React.PropsWithChildren) => {
+    const { status } = useAppSelector(selectUser);
+
     const dispatch = useAppDispatch();
 
-    dispatch(getUser());
-    return <>{children}</>;
+    useEffect(() => {
+        if (status === "refresh") {
+            dispatch(refreshUser());
+        }
+    }, [status]);
+
+    return children;
 });
 
 export default function ReduxProvider({ children }: React.PropsWithChildren) {
