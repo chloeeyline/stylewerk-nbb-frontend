ddf5452270bad5fe6f8ab63dd545b3516383dbfe
PolarBowler <39461054+PolarBowler@users.noreply.github.com>
2024-10-09 13:51:17 +0200

    cleanup select field

--- src/components/forms/SelectField.tsx
+++ src/components/forms/SelectField.tsx
@@ -2,36 +2,6 @@ import type React from "react";
 import { forwardRef, useId } from "react";
 import styles from "./form-fields.module.scss";
 
-/* export default function SelectField({
-    name,
-    label,
-    options,
-    state: { value, error },
-    ...props
-}: React.SelectHTMLAttributes<HTMLSelectElement> & {
-    name: string;
-    label: string;
-    options: [string, string][];
-    state: {
-        value: string;
-        error: string | null;
-    };
-}) {
-    const id = useId();
-
-    return (
-        <div className={styles.inputWrapper}>
-            <label htmlFor={`${name}-${id}`}>{label}</label>
-            <select name={`${name}-${id}`} value={value} {...props}>
-                {options.map(([key, value]) => (
-                    <option key={key}>{value}</option>
-                ))}
-            </select>
-            {error !== null ? <span>{error}</span> : null}
-        </div>
-    );
-} */
-
 export default forwardRef(function SelectField(
     {
         name,
