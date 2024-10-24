9f240c49e698ddb56ec01096244655d455e7c810
PolarBowler <39461054+PolarBowler@users.noreply.github.com>
2024-10-24 19:31:46 +0200

    add Logo

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from "react-i18next";
 import Grid from "~/components/layout/Grid";
 import ScrollContainer from "~/components/layout/ScrollContainer";
+import Logo from "~/components/Logo";
 
 export default function Home() {
     const { t } = useTranslation();
@@ -9,6 +10,7 @@ export default function Home() {
         <Grid layout="header" className="size-block-100">
             <h1>{t("nav.homepage")}</h1>
             <ScrollContainer direction="vertical">
+                <Logo className="m-i-auto m-b-3" style={{ inlineSize: "25ch" }} />
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime voluptatibus modi
                 dolorem, iusto error provident minus sit illo tenetur pariatur doloremque ullam.
                 Voluptatum eius placeat, magnam aliquam voluptas voluptatibus fuga.
