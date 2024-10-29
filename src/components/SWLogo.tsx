4394e39ae9902c2f528368fb5575a701528e8d3a
PolarBowler <39461054+PolarBowler@users.noreply.github.com>
2024-10-29 08:13:01 +0100

    add sw logo

---
+++ src/components/SWLogo.tsx
@@ -0,0 +1,55 @@
+import type { SVGProps } from "react";
+import { Ref, forwardRef } from "react";
+import styles from "./sw-logo.module.scss";
+import cls from "~/utils/class-name-helper";
+
+const SvgLogo = ({ className, ...props }: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
+    <svg
+        className={cls(className, styles.logo)}
+        xmlSpace="preserve"
+        viewBox="0 0 19.9417 10"
+        role="img"
+        aria-label="Stylewerk-Logo"
+        ref={ref}
+        {...props}>
+        <path
+            className={styles.drawBgS}
+            fill="none"
+            stroke="var(--clr-primary)"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M8.5 3h0a2 2 0 0 0-2-2H3a2 2 0 0 0-2 2h0a2 2 0 0 0 2 2h3.5a2 2 0 0 1 2 2h0a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2h0"
+            />
+        <path
+            className={styles.drawPathS}
+            fill="none"
+            stroke="var(--clr-header)"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="0.75"
+            d="M8.5 3h0a2 2 0 0 0-2-2H3a2 2 0 0 0-2 2h0a2 2 0 0 0 2 2h3.5a2 2 0 0 1 2 2h0a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2h0"
+            />
+        <path
+            className={styles.drawBgW}
+            fill="none"
+            stroke="var(--clr-accent)"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12.4417 1h0a1.5 1.5 0 0 0-1.5 1.5v5a1.5 1.5 0 0 0 1.5 1.5 1.5 1.5 0 0 0 1.5-1.5V6a1 1 0 0 1 1-1 1 1 0 0 1 1 1v1.5a1.5 1.5 0 0 0 1.5 1.5 1.5 1.5 0 0 0 1.5-1.5v-5a1.5 1.5 0 0 0-1.5-1.5h0"
+            />
+        <path
+            fill="none"
+            className={styles.drawPathW}
+            stroke="var(--clr-header)"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="0.75"
+            d="M12.4417 1h0a1.5 1.5 0 0 0-1.5 1.5v5a1.5 1.5 0 0 0 1.5 1.5 1.5 1.5 0 0 0 1.5-1.5V6a1 1 0 0 1 1-1 1 1 0 0 1 1 1v1.5a1.5 1.5 0 0 0 1.5 1.5 1.5 1.5 0 0 0 1.5-1.5v-5a1.5 1.5 0 0 0-1.5-1.5h0"
+        />
+    </svg>
+);
+
+const ForwardRef = forwardRef(SvgLogo);
+export default ForwardRef;
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 import Grid from "~/components/layout/Grid";
 import ScrollContainer from "~/components/layout/ScrollContainer";
 import Logo from "~/components/Logo";
+import SWLogo from "~/components/SWLogo";
 
 export default function Home() {
     const { t } = useTranslation();
@@ -10,7 +11,10 @@ export default function Home() {
         <Grid layout="header" className="size-block-100">
             <h1>{t("nav.homepage")}</h1>
             <ScrollContainer direction="vertical">
-                <Logo className="m-i-auto m-b-3" style={{ inlineSize: "25ch" }} />
+                <div className="d-flex gap-1 p-1" style={{ alignItems: "baseline", justifyContent: "center" }}>
+                    <Logo style={{ maxInlineSize: "25ch" }} />
+                    <SWLogo style={{ maxInlineSize: "7.5ch" }} />
+                </div>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime voluptatibus modi
                 dolorem, iusto error provident minus sit illo tenetur pariatur doloremque ullam.
                 Voluptatum eius placeat, magnam aliquam voluptas voluptatibus fuga.
